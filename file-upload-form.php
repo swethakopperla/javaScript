346244f6f5e2aa2d6cebfb9883438d8a54e4c5e5
umasaik <umasaik.itblabs@gmal.com>
2018-01-25 15:44:06 +0530

    added files

--- file-upload-form.php
+++ file-upload-form.php
@@ -8,7 +8,7 @@
     <form action="upload-manager.php" method="post" enctype="multipart/form-data">
         <h2>Upload File</h2>
         <label for="fileSelect">Filename:</label>
-        <input type="file" name="photo" id="fileSelect"
+        <input type="file" name="photo" id="fileSelect">
         <input type="submit" name="submit" value="Upload">
         <p><strong>Note:</strong> Only .jpg, .jpeg, .gif, .png formats allowed to a max size of 5 MB.</p>
     </form>
