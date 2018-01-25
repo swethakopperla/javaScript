5f56787fd1e0373228abb8e5e60de90d7d92f5a2
umasaik <umasaik.itblabs@gmal.com>
2018-01-25 15:50:49 +0530

    updated

--- upload-manager.php
+++ upload-manager.php
@@ -32,4 +32,13 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "Error: " . $_FILES["photo"]["error"];
     }
 }
+if($_FILES["photo"]["error"] > 0){
+    echo "Error: " . $_FILES["photo"]["error"] . "<br>";
+} else{
+    echo "File Name: " . $_FILES["photo"]["name"] . "<br>";
+    echo "File Type: " . $_FILES["photo"]["type"] . "<br>";
+    echo "File Size: " . ($_FILES["photo"]["size"] / 1024) . " KB<br>";
+    echo "Stored in: " . $_FILES["photo"]["tmp_name"];
+}
+
 ?>
