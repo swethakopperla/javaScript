874bf4974d771d51e5c80d7cbe866813b8953fe1
umasaik <umasaik.itblabs@gmal.com>
2018-01-25 11:40:43 +0530

    updated

---
+++ uploader.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php  
+$target_path = "/root/javaScript/";  
+$target_path = $target_path.basename( $_FILES['fileToUpload']['name']);   
+  
+if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $target_path)) {  
+    echo "File uploaded successfully!";  
+} else{  
+    echo "Sorry, file not uploaded, please try again!";  
+}  
+?>
+</body>
+</html>
