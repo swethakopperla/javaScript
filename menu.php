0d35824eb762e73ae46972050a13d9e797da5e63
umasaik <umasaik.itblabs@gmal.com>
2018-02-01 11:57:19 +0530

    added

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
 <html>
 <body>
 
-<a href="/file-upload-form.php">upload</a><br>
-<a href="/get-details-of-uploaded-file.php">upload details</a><br>
-<a href="/upload-manager.php">upload details</a><br>
+<a href="javaScript/file-upload-form.php">upload</a><br>
+<a href="javaScript/get-details-of-uploaded-file.php">upload details</a><br>
+<a href="javaScript/upload-manager.php">upload details</a><br>
 </body>
 </html>
 
