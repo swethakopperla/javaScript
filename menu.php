806ff5f43abf194e30f90e137350871e2f498d58
umasaik <umasaik.itblabs@gmal.com>
2018-02-01 11:54:32 +0530

    added

--- menu.php
+++ menu.php
@@ -1,10 +1,9 @@
 <html>
 <body>
-<?php
-echo "<a href="/file-upload-form.php">upload</a>"."<br>";
-echo "<a href="/get-details-of-uploaded-file.php">upload details</a>"."<br>";
-echo "<a href="/upload-manager.php">upload details</a>"."<br>";
-?>
+
+<a href="/file-upload-form.php">upload</a><br>
+<a href="/get-details-of-uploaded-file.php">upload details</a><br>
+<a href="/upload-manager.php">upload details</a><br>
 </body>
 </html>
 
